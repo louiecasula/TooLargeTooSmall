3b6d68ebd973cccd08336c8a5f07ad045bfdb282
Louie <louie@Zipcoders-MBP-3.fios-router.home>
2023-10-27 23:58:47 -0400

    finished

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,29 @@
+import java.util.Scanner;
+import java.util.Random;
+
 /**
  * Created by iyasuwatts on 10/17/17.
  */
 public class Main {
 
     public static void main(String[] args){
-        
+        Scanner in = new Scanner(System.in);
+        Random random = new Random();
+        int answer = random.nextInt(20) + 1;
+
+        while (true) {
+            System.out.print("Guess a number between 1 and 20: ");
+            int guess = in.nextInt();
+            if (guess == answer) {
+                System.out.println("You got it!");
+                break;
+            }
+            else if (guess > answer) {
+                System.out.println("Too high...");
+            }
+            else {
+                System.out.println("Too low...");
+            }
+        }
     }
 }
